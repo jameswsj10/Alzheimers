f14587a2edf0ff3382bcbf70135954d4ab5bf218
James Jung <jameswsj@berkeley.edu>
2019-07-27 00:21:01 -0700

    s

---
+++ reactAlz/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+	Header
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+	return 
+}
+
