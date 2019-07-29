5e3a894c91f6de5bd228816d462e1b8b7f66d22d
James Jung <jameswsj@berkeley.edu>
2019-07-28 17:52:57 -0700

    working on navigation

--- reactAlz/src/index.js
+++ reactAlz/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, {Component} from 'react';
 
 import { AppRegistry } from "react-native";
-import App from "./App.js";
-import { name as reactAlz } from "./reactAlz/App.json";  
-AppRegistry.registerComponent(reactAlz, () => App);
+import App from "/Users/jungwonsuk/Alzheimers/reactAlz/App";
+AppRegistry.registerComponent('reactAlz', () => App);
 
-import main from './src/main/main'
-import map from './src/map/map'
-import memories from './src/memories/memories'
+import main from '/Users/jungwonsuk/Alzheimers/reactAlz/src/main/main'
+import map from '/Users/jungwonsuk/Alzheimers/reactAlz/src/map/map'
+import memories from '/Users/jungwonsuk/Alzheimers/reactAlz/src/memories/memories'
 
-import { colors } from './src/theme'
+import { colors } from '/Users/jungwonsuk/Alzheimers/reactAlz/src/theme'
 
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
 
